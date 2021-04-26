dd11190e5aa11c5c05edfa5695d884d98abb4e44
Barry Cswords <941436+cswords@users.noreply.github.com>
2021-04-26 20:05:45 +1000

    Update dbscan.m

--- dbscan.m
+++ dbscan.m
@@ -1,30 +1,31 @@
-function [ class ] = dbscan(dis, eps, minPts)
-
-max_dis = max(max(dis));
-if max_dis > 1
-    dis = dis/max_dis;
-end
+function [ class ] = DBSCAN(dis, eps, minPts)
 
 m_adjacency = dis <= eps;
 l_core = sum(m_adjacency, 2) >= minPts;
 m_adjacency_core = m_adjacency(l_core, l_core);
 
-class = zeros(size(dis, 1), 1);
+class = zeros(size(dis, 1), 1)-1;
 
 if isempty(m_adjacency_core)
-    return
+    return % No core, all noise
 end
 
 g_dbscan = graph(gather(m_adjacency_core));
+% figure
+% plot(g_dbscan);
 class_core = conncomp(g_dbscan);
 
-[~, l_assign_core] = min(dis(l_core, :));
+[l_assign_dis, l_assign_core] = min(dis(l_core, :));
 
 if size(class_core) == [1, 1]
     class = class + 1;
     return
 end
 
-class = class_core(l_assign_core)';
+class = class_core(l_assign_core);
+class(l_assign_dis > eps) = -1;
+
+
+class = class';
 
 end
