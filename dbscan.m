e00145b5a9b1f09c879a64f010da311a19bdd616
Barry Cswords <941436+cswords@users.noreply.github.com>
2021-04-26 22:16:27 +1000

    Update dbscan.m

--- dbscan.m
+++ dbscan.m
@@ -4,9 +4,8 @@ m_adjacency = dis <= eps;
 l_core = sum(m_adjacency, 2) >= minPts;
 m_adjacency_core = m_adjacency(l_core, l_core);
 
-class = zeros(size(dis, 1), 1) - 1;
-
 if isempty(m_adjacency_core)
+    class = zeros(size(dis, 1), 1) - 1;
     return % No core, all noise
 end
 
